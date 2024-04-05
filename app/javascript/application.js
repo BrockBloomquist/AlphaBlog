e41914cc6d52b1336abbe20d21ff768885172268
Brock Bloomquist <brockbloomquist@gmail.com>
2024-04-05 11:47:01 -0500

    Initial Commit

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+  return new bootstrap.Popover(popoverTriggerEl)  
+})  
