51108d05c0d99b4f1c6c7562a41b96779f33ebae
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-17 15:02:47 -0500

    Added add_column to migration for emails for Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_15_192457) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_195600) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_192457) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
