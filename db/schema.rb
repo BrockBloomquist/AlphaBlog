6803b405b28c47d0235a9de6ce7cd7cc09e6b1b3
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-17 11:00:48 -0500

    Created a migration for the articles table and created the users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_05_163357) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_192457) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
