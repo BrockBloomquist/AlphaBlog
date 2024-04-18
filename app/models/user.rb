3ee0ca82b444594a81e0bd50f226b167104cead4
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-17 21:58:53 -0500

    Added user info to articles and some styling for icons. Removed unnecessary links on navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, presence: true, 
                             uniqueness: {case_sensitive: false }, 
                             length: {minimum: 3, maximum: 25}
@@ -7,6 +8,4 @@ class User < ApplicationRecord
                                     uniqueness: {case_sensitive: false }, 
                                     length: {maximum: 105},
                                     format: { with: VALID_EMAIL_REGEX }
-
-    has_many :articles
 end
