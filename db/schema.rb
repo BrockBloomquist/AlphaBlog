37fbd3e4d63deaa2162840f1211844a5236bce59
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-18 15:37:45 -0500

    Added BCrypt gem for authentication and created password digest migration for the Users model/table to incorporate authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_195600) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_192129) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_195600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
