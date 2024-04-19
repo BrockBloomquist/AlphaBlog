f5df14343436fd45f5d0de216e0ec7cee911937c
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-19 16:48:03 -0500

    Cleaned up some files. Added some classes to the edit page to make the footer stick to the bottom.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.html { redirect_to user_url(@user), notice: "Your account information was succesfully updated. " }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
