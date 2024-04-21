100e50b419f28188e737385ec7c54ee2d08692a4
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-21 18:24:32 -0500

    Created login form and created routes for login GET and POST and the DELETE for the route logout for authentication implementation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :users
   resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
 
-  # Defines the root path route ("/")
   root "pages#home"
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
 end
