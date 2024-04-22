74372d2e070880c6077ead646d2f447140c85203
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-22 10:17:11 -0500

    Finished the new, create, and destroy actions for authentication in the sesssions controller file using ActiveRecord cookie sessions to store user ids

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
-  
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
