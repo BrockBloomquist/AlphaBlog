15fb5144344f3368a98c45b46ec442451633f684
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-23 12:52:46 -0500

    Created controller methods in the application controller and used the helper_method to allow view usage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
 
     respond_to do |format|
       if @article.save
