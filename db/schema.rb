8136fc22dec93bdc9348c9a0b5f2bc4fcce85d50
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-23 21:43:01 -0500

    Created and ran migration for users to have an admin column in its table-introduction to admin priviledges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_192129) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_023637) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_192129) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
