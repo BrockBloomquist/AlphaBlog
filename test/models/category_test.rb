4939bc878c81f080d321761773a5bdb9f752b68a
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-25 15:31:57 -0500

    Created new model category for articles, model testing for it, need to figure out how to make the controller tests work

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,5 +1,8 @@
 require 'test_helper'
 
-class CategoryTest < ActiveSupport
-  
+class CategoryTest < ActiveSupport::TestCase
+    test "category should be valid" do
+      @category = Category.new(name: "Sports")
+      assert @category.valid?
+    end
 end
