045e766f2330adc48921f75732f3b64b18efafe8
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-29 13:07:00 -0500

    Created new and create methods for Categories controller with all tests functioning as needed for the controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,20 @@
 class CategoriesController < ApplicationController
 
     def new 
-      
+      @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        respond_to do |format|
+            if @category.save
+                format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+                format.json { render :show, status: :created, location: @category }
+            else 
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @category.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def index
@@ -12,4 +25,9 @@ class CategoriesController < ApplicationController
       
     end
 
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
