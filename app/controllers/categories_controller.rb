ce9132f14ed5d6d2db3d323e97c5726cd9313c0b
BrockBloomquist <brockbloomquist1@gmail.com>
2024-04-29 13:44:34 -0500

    Added integration tests for the show page for the categories. Created set category private controller method for DRY practice

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show]
 
     def new 
       @category = Category.new
@@ -26,6 +27,10 @@ class CategoriesController < ApplicationController
     end
 
     private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
 
     def category_params
       params.require(:category).permit(:name)
