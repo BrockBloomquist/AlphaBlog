b3bd15235c462eada49a329a0a3dbd5d7a6e1230
BrockBloomquist <brockbloomquist1@gmail.com>
2024-05-02 17:16:58 -0500

    Finished project development. Need more tests and QA and soft deploy!

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  before_action :require_admin, except: %i[index show]
-  before_action :set_category, only: [:show]
+  before_action :require_admin, except: %i[index show edit update]
+  before_action :set_category, only: %i[show edit update]
 
   def new
     @category = Category.new
   end
+
+  def edit; end
 
   def create
     @category = Category.new(category_params)
@@ -21,11 +23,25 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), notice: 'Category has been successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: 300 }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
-  def show; end
+  def show
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   private
 
